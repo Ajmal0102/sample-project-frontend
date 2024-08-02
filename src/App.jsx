0b2d9d46f2fe5586c4f5957761ff737063028e34
Ajmal0102 <ajmalsajeer362@gmail.com>
2024-08-02 12:13:11 +0530

    install react js and tailwind css and create the first template

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+    <h1 className='text-blue-800 hover:text-blue-500 cursor-pointer'>
+      This Is My One Of The Important Project For Leaning Purpose.
+    </h1>
+    </>
+  )
+}
+
+export default App
